58d4b2d9b07dbb11e39e4765cf37e56565eb9dae
hmatsuo <hiroki.matsuo@nts.ricoh.co.jp>
2016-02-18 16:01:50 +0900

    “第一章練習問題2,4をコミット”

---
+++ jpl/src/ch01/ex02/HellowWorldError.java
@@ -0,0 +1,12 @@
+package ch01.ex02;
+
+public class HellowWorldError {
+
+	public static void main(String[] args) {
+		/* 構文エラー */
+		//System.out.println("Hellow, world")
+
+		System.out.println("Hellow, world");
+	}
+
+}
---
+++ jpl/src/ch01/ex04/SquareNumber.java
@@ -0,0 +1,14 @@
+package ch01.ex04;
+
+public class SquareNumber {
+
+	public static void main(String[] args) {
+		//タイトルを出力
+		System.out.println("\"平方表\"");
+
+		for (int i = 0; i < 10; i++) {
+			System.out.println(i * i);
+		}
+	}
+
+}
